f50466163750dedb3b4c17e146720b147bef8ac3
szy4211 <szy4211@users.noreply.github.com>
2019-06-22 11:44:36 +0800

    Create Des.php
    使用openssl进行des加密

---
+++ Des.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @description: des对称加密
+ *
+ * @date 2019-06-21
+ * author zornshuai@foxmail.com
+ */
+
+namespace App\Services;
+
+
+class Des
+{
+    protected $key;
+    protected $iv;
+
+    /**
+     * Des constructor.
+     * @param string $key
+     * @param string $iv 8位
+     */
+    public function __construct($key, $iv = '00000000')
+    {
+        $this->key = $key;
+        $this->iv  = $iv;
+    }
+
+    public function encrypt($data)
+    {
+        return openssl_encrypt($data, 'des-cbc', $this->key, 0, $this->iv);
+    }
+
+    public function decrypt($data)
+    {
+        return openssl_decrypt($data, 'des-cbc', $this->key, 0, $this->iv);
+    }
+}
